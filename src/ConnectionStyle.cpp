aca9fa5d8a1443c825d0c1db58b07941ef812a75
Qv2ray-dev <59914293+Qv2ray-dev@users.noreply.github.com>
2020-06-22 09:13:20 +0800

    fix: fixed qsrand and qrand warning

--- src/ConnectionStyle.cpp
+++ src/ConnectionStyle.cpp
@@ -122,8 +122,8 @@ QColor ConnectionStyle::normalColor(QString typeId) const
 {
     std::size_t hash = qHash(typeId);
     std::size_t const hue_range = 0xFF;
-    qsrand(hash);
-    std::size_t hue = qrand() % hue_range;
+    std::srand(hash);
+    std::size_t hue = std::rand() % hue_range;
     std::size_t sat = 120 + hash % 129;
     return QColor::fromHsl(hue, sat, 160);
 }
